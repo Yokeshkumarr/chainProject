ae58f186e13940e7c13a69e79add4bc7c92c27c6
Yokeshkumarr <157039333+Yokeshkumarr@users.noreply.github.com>
2025-02-03 19:01:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+function Adding(){
+ let a=document.getElementById("loginPa").style.display="block";
+}
+function reming(){
+ let a=document.getElementById("loginPa").style.display="none";
+}
+
+
+// 
+
